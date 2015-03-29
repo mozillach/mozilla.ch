47d85425fe640037882ec9284e9e89310afe246d
Martin Giger <martin.a.giger@sunrise.ch>
2015-03-29 12:32:08 +0200

    Fix #14 by watching the problematic tiles for size changes

--- app/Resources/public/scripts/setup-tiles.js
+++ app/Resources/public/scripts/setup-tiles.js
@@ -1,6 +1,13 @@
 window.addEventListener("load", function() {
-  new Masonry(document.querySelector('.promo-grid'), {
+  var masonry = new Masonry(document.querySelector('.promo-grid'), {
     itemSelector : '.item',
     columnWidth : 160
   });
+
+  // watch the wide tiles for size changes, so the grid doesn't break.
+  var wideTiles = document.querySelectorAll(".promo-large-landscape"),
+      layout = function() { masonry.layout(); };
+  for(var i = 0; i < wideTiles.length; ++i) {
+    new ResizeSensor(wideTiles[i], layout);
+  }
 });
