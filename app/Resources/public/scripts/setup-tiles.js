a355170ae42a6841206d49a35aa37144b1dc46dc
Martin Giger <martin.a.giger@sunrise.ch>
2015-03-29 11:55:11 +0200

    Get rid of explicit jQuery

--- app/Resources/public/scripts/setup-tiles.js
+++ app/Resources/public/scripts/setup-tiles.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $('.promo-grid').masonry({
+window.addEventListener("load", function() {
+  new Masonry(document.querySelector('.promo-grid'), {
     itemSelector : '.item',
     columnWidth : 160
   });
