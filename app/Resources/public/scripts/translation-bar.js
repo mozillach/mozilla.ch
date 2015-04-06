35b0bd5c2a53dfa5d5fb2a10aa97d951e96b1728
Martin Giger <martin.a.giger@sunrise.ch>
2015-04-06 21:36:26 +0200

    Make a transbar, that ignores the -CH

---
+++ app/Resources/public/scripts/translation-bar.js
@@ -0,0 +1,83 @@
+window.addEventListener("load", function() {
+    var transbar = new window.Tabzilla.infobar('transbar', 'Translation Bar');
+
+    var userLangs = navigator.languages;
+    var pageLang = document.documentElement.lang;
+
+    if (transbar.disabled || !userLangs || !pageLang) {
+        console.log("Nothing to do here");
+        return false;
+    }
+
+    // Normalize the user language in the form of ab or ab
+    var normalize = function (lang) {
+        return lang.replace(/^(\w+)(?:-\w+)?$/, function (m, p1) {
+            return p1.toLowerCase();
+        });
+    };
+
+    // Normalize every language for easier comparison
+    userLangs = $.map(userLangs, function (lang) { return normalize(lang); });
+    pageLang = normalize(pageLang);
+
+    // If the page language is the user's primary language, there is nothing
+    // to do here
+    if (pageLang === userLangs[0]) {
+        console.log("Already on primary lang");
+        return false;
+    }
+
+    var availableLangs = [];
+    var $links = $('link[hreflang]');
+
+    if ($links.length) {
+        $links.each(function () {
+            availableLangs[normalize(this.hreflang)] = this;
+        });
+    }
+
+    // Compare the user's accept languages against the page's current
+    // language and other available languages to find the best language
+    $.each(userLangs, function(index, userLang) {
+        if (pageLang === userLang) {
+            offeredLang = 'self';
+            return false; // Break the loop
+        }
+
+        if (userLang in availableLangs) {
+            offeredLang = userLang;
+            return false; // Break the loop
+        }
+    });
+
+    // If the page language is one of the user's secondary languages and no
+    // other higher-priority language cannot be found in the translations,
+    // there is nothing to do
+    if(offeredLang == 'self' || !offeredLang) {
+        console.log("There's no better lang");
+        return false;
+    }
+
+    // Do not show Chrome's built-in Translation Bar    
+    $('head').append('<meta name="google" value="notranslate">');
+
+    transbar.onaccept = {
+        trackAction: '',
+        trackLabel: offeredLang,
+        callback: function () {
+            var element = availableLangs[offeredLang];
+
+            location.href = element.href;
+        }
+    };
+
+    // Fetch the localized strings and show the Translation Bar
+    $.ajax({ url: 'https://mozorg.cdn.mozilla.net/' + offeredLang + '/tabzilla/transbar.jsonp',
+             cache: true, crossDomain: true, dataType: 'jsonp',
+             jsonpCallback: "_", success: function (str) {
+        transbar.show(str).attr({
+            'lang': offeredLang,
+            'dir': ($.inArray(offeredLang, ['he', 'ar', 'fa', 'ur']) > -1) ? 'rtl' : 'ltr'
+        });
+    }});
+});
