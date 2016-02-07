d3d0e110e719620f7610a3348ea3fc1716d94f5f
Colin Frei <colin@colinfrei.com>
2016-02-07 22:30:42 +0100

    let's see how much of this we can remove

--- start.sh
+++ start.sh
@@ -1,19 +1,3 @@
 #! /bin/sh
 
-# Install vendor deps
-composer.phar install --no-dev --optimize-autoloader
-
-# Run symfony server checks
-php app/check.php
-
-# Warm up cache
-php app/console cache:warmup --env=prod --no-debug
-
-# Dump assetic assets
-php app/console assetic:dump --env=prod --no-debug
-
-# Make cache dir writeable n stuff
-chown -R www-data:www-data app
-chmod -R a+rw app/cache app/logs
-
 apache2-foreground
