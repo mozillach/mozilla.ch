6f36c73e8443bb729ba52bfe3c0bd06848a3a21c
Colin Frei <colin@colinfrei.com>
2016-02-13 08:25:09 +0100

    try not warming up the cache, so that the webserver doesn't have permission issues

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 # Run composer scripts, since we have the ENV variable now
 composer.phar run-script post-install-cmd
 
-# Warm up cache
-php app/console cache:warmup --env=prod --no-debug
+php app/console cache:clear --env=prod --no-debug --no-warmup
 
 # Dump assetic assets
 php app/console assetic:dump --env=prod --no-debug
